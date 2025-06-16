842b958131ed6e6a783fcea11ccb2fbe4d2495f1
ayush singh <ayushs24101@iiitnr.edu.in>
2025-06-16 15:07:37 +0530

    initial commit

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: [
+      "http://127.0.0.1:5173",
+      "http://localhost:5173",
+      "https://vet-mate.vercel.app",
+    ],
     credentials: true,
   })
 );
